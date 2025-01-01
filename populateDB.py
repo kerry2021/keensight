d52b650eee7f8f0ba824c4f060533075d962f7d4
kerry2021 <aca.kerry.li@gmail.com>
2024-12-31 22:39:34 -0500

    job collection experiement
    experimentation with collecting job data from internet

---
+++ populateDB.py
@@ -0,0 +1,19 @@
+from jobspy import scrape_jobs
+
+jobs = scrape_jobs(
+    site_name=["indeed", "linkedin", "zip_recruiter", "glassdoor", "google"],
+    search_term="software engineer",
+    google_search_term="software engineer jobs near San Francisco, CA since yesterday",
+    location="San Francisco, CA",
+    results_wanted=20,
+    hours_old=72,
+    country_indeed='USA',
+    
+    # linkedin_fetch_description=True # gets more info such as description, direct job url (slower)
+    # proxies=["208.195.175.46:65095", "208.195.175.45:65095", "localhost"],
+)
+print(f"Found {len(jobs)} jobs")
+print(jobs.head())
+for index, row in jobs.iterrows():
+    for key, value in row.items():
+        print(key, value)
